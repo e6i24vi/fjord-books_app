ceb5504943a17b2772e85b58df6458ab6196fb72
e6i24v <e6i24v@gmail.com>
2021-10-05 07:11:35 +0900

    add:課題提出

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,25 @@ class User < ApplicationRecord
 
   validates :uid, uniqueness: { scope: :provider }, if: -> { uid.present? }
 
+  has_one_attached :avatar
+
+  validate :avatar_type
+
   def self.from_omniauth(auth)
     find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
       user.name = auth.info.name
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
     end
+  end
+
+  private
+
+  def avatar_type
+    return if avatar.blob.content_type.in?(%('image/jpeg image/png image/gif'))
+
+    avatar = nil
+    errors.add(:avatar, 'はjpegまたはpngまたはgif形式でアップロードしてください')
+    avatar
   end
 end
