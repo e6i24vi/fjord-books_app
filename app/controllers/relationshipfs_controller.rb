e22f528cab4cfb3023eea67f9c4952ad1de86d63
e6i24v <e6i24v@gmail.com>
2021-12-16 06:44:32 +0900

    redirect_to 先の修正 || root_path 削除

--- app/controllers/relationshipfs_controller.rb
+++ app/controllers/relationshipfs_controller.rb
@@ -4,12 +4,12 @@ class RelationshipfsController < ApplicationController
   def create
     following = current_user.relationshipfs.build(follower_id: params[:user_id])
     following.save!
-    redirect_to  user_path(params[:user_id]) || root_path
+    redirect_to  user_path(params[:user_id])
   end
 
   def destroy
     following = current_user.relationshipfs.find_by!(follower_id: params[:user_id])
     following.destroy!
-    redirect_to user_path(params[:user_id]) || root_path
+    redirect_to user_path(params[:user_id])
   end
 end
